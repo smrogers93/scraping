e22ac1d462efd2b6e6da69a68e3bb58d2cedc055
Stephen Rogers <stephenrogers@iMac-1548.local>
2019-08-16 09:23:57 -0400

    config

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
